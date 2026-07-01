8b78e26deff4e76ad79ca847a02a17e0a6fd731c
henribon <henribonrec@gmail.com>
2026-06-30 21:59:12 -0300

    innitial commit project

---
+++ src/main/java/com/goose/mipas/MipasApplication.java
@@ -0,0 +1,13 @@
+package com.goose.mipas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MipasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MipasApplication.class, args);
+	}
+
+}
